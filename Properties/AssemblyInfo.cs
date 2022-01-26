539dbec070a7d430392f4ff864c344287f69c3b5
thepicture <85333076+thepicture@users.noreply.github.com>
2022-01-26 07:07:01 +0000

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WSUniversalLib")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WSUniversalLib")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fd4622f6-8ff0-4e8e-9acb-5b3dcb869c2e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WSUniversalLib
+{
+    [TestClass]
+    public class Calculation
+    {
+        [TestMethod]
+        public int GetQuantityForProduct(int productType,
+                                         int materialType,
+                                         int count,
+                                         float width,
+                                         float length)
+        {
+            return default;
+        }
+    }
+}
