825d1f64c78e1773b8471900b5abfe7b5256e9fd
thepicture <85333076+thepicture@users.noreply.github.com>
2022-01-26 08:07:40 +0000

    Write 10 easy unit tests

--- CalculationTests.cs
+++ CalculationTests.cs
@@ -8,7 +8,7 @@ namespace WSUniversalLib
         private static readonly Calculation calculation = new Calculation();
 
         [TestMethod]
-        public void GetQuantityForProduct_ValidParams_ReturnsInteger()
+        public void GetQuantityForProduct_ValidParamsEasy_ReturnsInteger()
         {
             // Arrange.
             int expected = 114148;
@@ -19,5 +19,122 @@ namespace WSUniversalLib
             // Assert.
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void GetQuantityForProduct_NonExistentProductTypeEasy_ReturnsNegativeOne()
+        {
+            // Arrange.
+            int expected = -1;
+
+            // Act.
+            int actual = calculation.GetQuantityForProduct(0, 1, 15, 20, 45);
+
+            // Assert.
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetQuantityForProduct_NonExistentMaterialTypeEasy_ReturnsNegativeOne()
+        {
+            // Arrange.
+            int expected = -1;
+
+            // Act.
+            int actual = calculation.GetQuantityForProduct(1, 0, 15, 20, 45);
+
+            // Assert.
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetQuantityForProduct_NegativeCountEasy_ReturnsNegativeOne()
+        {
+            // Arrange.
+            int expected = -1;
+
+            // Act.
+            int actual = calculation.GetQuantityForProduct(1, 1, -1, 20, 45);
+
+            // Assert.
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetQuantityForProduct_NegativeWidthEasy_ReturnsNegativeOne()
+        {
+            // Arrange.
+            int expected = -1;
+
+            // Act.
+            int actual = calculation.GetQuantityForProduct(1, 1, 1, -5, 45);
+
+            // Assert.
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetQuantityForProduct_NegativeLengthEasy_ReturnsNegativeOne()
+        {
+            // Arrange.
+            int expected = -1;
+
+            // Act.
+            int actual = calculation.GetQuantityForProduct(1, 1, 1, 5, -45);
+
+            // Assert.
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetQuantityForProduct_NegativeLengthAndWidthEasy_ReturnsNegativeOne()
+        {
+            // Arrange.
+            int expected = -1;
+
+            // Act.
+            int actual = calculation.GetQuantityForProduct(1, 1, 1, -5, -45);
+
+            // Assert.
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetQuantityForProduct_ZeroWidth_ReturnsNegativeOne()
+        {
+            // Arrange.
+            int expected = -1;
+
+            // Act.
+            int actual = calculation.GetQuantityForProduct(1, 1, 1, 0, 45);
+
+            // Assert.
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetQuantityForProduct_ZeroLength_ReturnsNegativeOne()
+        {
+            // Arrange.
+            int expected = -1;
+
+            // Act.
+            int actual = calculation.GetQuantityForProduct(1, 1, 1, 1, 0);
+
+            // Assert.
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void GetQuantityForProduct_ZeroCount_ReturnsNegativeOne()
+        {
+            // Arrange.
+            int expected = -1;
+
+            // Act.
+            int actual = calculation.GetQuantityForProduct(1, 1, 0, 1, 1);
+
+            // Assert.
+            Assert.AreEqual(expected, actual);
+        }
+
     }
 }
